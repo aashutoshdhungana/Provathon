99de22af63961834a7ec45653297ee48184838e7
Kirtee011 <kirteemaharjan123@gmail.com>
2021-07-06 15:35:21 +0545

    fixing

---
+++ homestay-client/src/componenet/Homestay/homestayProfile.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { homestaysDemo } from "../../demo/homestay-demo";
+
+const HomestayProfile = () => {
+  return (
+    <div>
+      <div class="homestay profile banner"></div>
+    </div>
+  );
+};
