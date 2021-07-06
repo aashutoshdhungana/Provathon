56d9188c98647d8082c180713e77551a9ecaf6e2
Pradip Lamichhane <pradip.lamichhane1102@gmail.com>
2021-07-06 12:56:48 +0545

    navbar

--- homestay-client/src/componenet/Navbar.js
+++ homestay-client/src/componenet/Navbar.js
@@ -1,12 +1,11 @@
 import react from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
+
 
 function Navbar()
 {
     return(
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light text-light bg-dark">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
