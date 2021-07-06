19d2ae3a1b0ba37bd02be8f1d4a6d06c75ac127d
Nishesh <nishesh.ac007@gmail.com>
2021-07-06 14:19:55 +0545

    this is homestay login and register

--- homestay-client/src/componenet/App.js
+++ homestay-client/src/componenet/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Register from "./Register";
 import Login from "./Login";
+import Hsregister from "./Hsregister";
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div>
       {/* <Register /> */}
       {/* <Login /> */}
+      <Hsregister/>
     </div>
   );
 }
