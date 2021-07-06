a86efb6838cdbd8bb5c75aabdb38a6c48ae75786
Nishesh <nishesh.ac007@gmail.com>
2021-07-06 11:30:34 +0545

    this is register page

--- homestay-client/src/componenet/App.js
+++ homestay-client/src/componenet/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Register from "./Register";
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Register />
     </div>
   );
 }
