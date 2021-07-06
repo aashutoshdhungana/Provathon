52c9d5ff3c2809b391e68c402cc9ef542d5d95e1
Pradip Lamichhane <pradip.lamichhane1102@gmail.com>
2021-07-07 05:20:12 +0545

    search and bookmark

--- homestay-client/src/componenet/App.js
+++ homestay-client/src/componenet/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Routers from "./Router/Routers";
+// import Routers from "./Router/Routers";
+import Routers from './Router/Routers';
 
 function App() {
   return (
     <div>
       <Routers />
+      
     </div>
   );
 }
